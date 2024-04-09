b846a85ae3c24fec59cdbf686088814fc330cb7b
Angel Gamarra <angelgcrz@gmail.com>
2024-04-09 16:06:24 -0500

    modelo creado

---
+++ models/Usuario.js
@@ -0,0 +1,20 @@
+import { DataTypes } from 'sequelize'
+import db from '../config/db.js'
+const usuario = db.define('usuario', {
+    nombre:{
+        type: DataTypes.STRING,
+        allowNull: false, //este campo no puede ir vacio
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    token: DataTypes.STRING,
+    confirmado: DataTypes.BOOLEAN
+})
+
+export default Usuario
